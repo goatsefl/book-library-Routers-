24b4b2bbf693858dec1b61f1df47241c31ce6d79
goatsefl <swaroopd502@gmail.com>
2025-01-27 23:48:59 +0530

    New push

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -4,11 +4,10 @@ import { useParams } from "react-router-dom";
 
 
 export default function BookDetails() {
-    const bookId = useParams()
-    const books = useContext(BooksContext)
-    const book = books.find(book => book.id == bookId)
-    useEffect(() => {
-        console.log(bookId)
-    })
+    const { bookId } = useParams() // This here is used to make bookId as one of the parameters in the address.     
+    const books = useContext(BooksContext) // In the books there's a fetch 
+    const book = books.find(book => book.id === parseInt(bookId))
+
+
     return (<></>)
 }
