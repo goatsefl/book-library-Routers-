10160cff6723a08b6e3a7e016241ae328aa738a4
goatsefl <swaroopd502@gmail.com>
2025-01-28 22:40:44 +0530

    Added query search, one error pending :'List All Books'

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Books from './components/Books';
 import { BooksProvider } from './BooksContext';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import BookDetails from './components/BookDetails';
+import NotFound from './components/NotFound';
 
-// BrowseRouter 
+// The asterisk(*) implies in the path apart from any route provided below, if there's none matching, it'll be routed.
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={<Books></Books>}></Route>
           <Route path='/books' element={<Navigate to='/' />}></Route>
           <Route path='/books/:bookId' element={<BookDetails></BookDetails>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
         </Routes>
       </Router>
     </BooksProvider>
--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,13 +1,25 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { BooksContext } from "../BooksContext";
 import { useParams } from "react-router-dom";
 
 
 export default function BookDetails() {
     const { bookId } = useParams() // This here is used to make bookId as one of the parameters in the address.     
-    const books = useContext(BooksContext) // In the books there's a fetch 
+    const books = useContext(BooksContext) // In the BooksContext.js, there's a fetch for books.json. This context is used here. 
     const book = books.find(book => book.id === parseInt(bookId))
 
+    if (books.length === 0) {
+        return (<div>Loading...</div>)
+    }
 
-    return (<></>)
+    if (!book) {
+        return (<div><h1>Book not found!</h1></div>)
+    }
+    return (
+        <div>
+            <h1>{book.id}. {book.title}</h1>
+            <h2>by {book.author}</h2>
+            <p>{book.description}</p>
+        </div>
+    )
 }
